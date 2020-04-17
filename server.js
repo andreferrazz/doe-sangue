f4c68bbd38193e18a12fd170b2cd752b228c482a
André Ferraz <andreferraz25384@gmail.com>
2020-04-17 13:45:23 -0300

    update dependencies

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(express.urlencoded({ extended: true }))
 const Pool = require('pg').Pool
 const db = new Pool({
     user: 'postgres',
-    password: 'password',
+    password: 'asdf',
     host: 'localhost',
     port: 5432,
     database: 'doe'
